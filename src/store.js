7ce042dd9b7ed89b7079a37cf1f0baca0161bff9
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-19 15:52:21 +0500

    Create store.js

---
+++ src/store.js
@@ -0,0 +1,12 @@
+// src/store.js
+
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "./store/users/usersSlice";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+});
+
+export default store;
